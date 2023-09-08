a54c78944caccfb57ab4970a55eef70ddbc020be
SHIELD660 <richmondrichmond183@gmail.com>
2023-09-08 13:56:56 +0100

    Create SIM-SWAP.sh
    SIM SWAP

---
+++ SIM-SWAP.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Define the get_headers function
+function get_headers() {
+  # Get the response from the API
+  response=$(curl -k -s -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9" 'http://192.168.8.1/api/ussd/send')
+
+  # Extract the cookie and token from the headers
+  cookie=$(echo "$response" | grep -E '^Set-Cookie: ' | cut -d ';' -f1 | cut -d '=' -f2)
+  token=$(echo "$response" | grep -E '__RequestVerificationToken:' | cut -d '=' -f2)
+
+  # Return the cookie and token
+  echo "$cookie" "$token"
+}
+
+# Define the get_curl_command function
+function get_curl_command() {
+  # Get the function arguments
+  local url=$1
+  local cookie=$2
+  local token=$3
+
+  # Return the curl command
+  echo "curl -X POST '$url' -H 'Cookie: $cookie' -H 'x-requested-with: XMLHttpRequest' -H '__RequestVerificationToken: $token'"
+}
+
+# Define the execute_curl_commands function
+function execute_curl_commands() {
+  # Get the curl commands
+  curl_commands=$(
+    get_curl_command 'Select the option for SIM swap' "$cookie" "$token"
+    get_curl_command 'Enter your old MTN Cameroon phone number' "$cookie" "$token"
+    get_curl_command 'Enter your National ID card number or passport number' "$cookie" "$token"
+    get_curl_command 'Enter the IMEI number of your phone' "$cookie" "$token"
+    get_curl_command 'Enter the serial number of your old SIM card' "$cookie" "$token"
+    get_curl_command 'Click on the "Submit" button' "$cookie" "$token"
+  )
+
+  # Execute the curl commands
+  for curl_command in $curl_commands; do
+    echo "$curl_command"
+    curl -s "$curl_command" > /dev/null
+  done
+}
+
+# Get the cookie and token
+cookie, token=$(get_headers)
+
+# Get the curl commands
+curl_commands=$(
+  get_curl_command 'Select the option for SIM swap' "$cookie" "$token"
+  get_curl_command 'Enter your old MTN Cameroon phone number' "$cookie" "$token"
+  get_curl_command 'Enter your National ID card number or passport number' "$cookie" "$token"
+  get_curl_command 'Enter the IMEI number of your phone' "$cookie" "$token"
+  get_curl_command 'Enter the serial number of your old SIM card' "$cookie" "$token"
+  get_curl_command 'Click on the "Submit" button' "$cookie" "$token"
+)
+
+# Execute the curl commands
+execute_curl_commands "$curl_commands"
